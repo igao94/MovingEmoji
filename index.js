c94a86c6eebcb4be90befcc8c30bf7eab4b7ce3c
igao94 <igao998@gmail.com>
2024-02-11 15:33:26 +0100

    emoji

---
+++ index.js
@@ -0,0 +1,38 @@
+const box = document.getElementById("box");
+const moveAmount = 20;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", (event) => {
+  box.style.backgroundColor = "tomato";
+  box.textContent = "🤬";
+});
+
+document.addEventListener("keyup", (event) => {
+  box.style.backgroundColor = "lightblue";
+  box.textContent = "😊";
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key.startsWith("Arrow")) {
+    event.preventDefault();
+
+    switch (event.key) {
+      case "ArrowUp":
+        y -= moveAmount;
+        break;
+      case "ArrowDown":
+        y += moveAmount;
+        break;
+      case "ArrowLeft":
+        x -= moveAmount;
+        break;
+      case "ArrowRight":
+        x += moveAmount;
+        break;
+    }
+
+    box.style.top = `${y}px`;
+    box.style.left = `${x}px`;
+  }
+});
